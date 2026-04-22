3f46810c18a5ef6cc93a380581e91e8709326e36
maria-lorenna <202430333765@estudante.rn.gov.br>
2026-04-22 11:43:39 +0000

    Aula de Revisão

---
+++ revisao/revisao.js
@@ -0,0 +1,25 @@
+// Revisão de PEOO
+//Declaração de variáveis
+
+// Var -> Está descontinuado
+var nome = "Joel Santos"; //tipo texto
+
+// Let -> Está em uso atualmente (mais comum), pode ser reatribuído
+let idade = 20;
+idade = 50;
+idade = "Cinquenta";
+
+// const -> constante (variavel fixa)
+const cidade = "Ielmo";
+//cidade = "Ielmo Marinho";
+
+console.log(cidade);
+
+// Manipulação e operações
+// = -> atribuição
+// + soma, - subtração, * multiplicação, / divisão, % resto da divisão;
+let valor = 10;
+valor = 10 + 10;
+console.log(valor);
+valor += 30;
+console.log(valor);
